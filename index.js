12dc1aaba9ea44c4c294b3fe9593fe227325dc09
Colin Leung <colin@cowise.co>
2024-10-13 11:37:27 +0800

    feat: v2 filename and configs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import url from "node:url";
-const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
-export default {dirname}
+export default {
+  filename: import.meta.url,
+  configs: ['form']
+}
 
 import MultipartParser from './classes/MultipartParser.mjs';
 import ControllerMixinMultipartForm from './classes/controller-mixin/MultipartForm.mjs';
--- init.mjs
+++ init.mjs
@@ -1,6 +1,1 @@
-import { Central } from '@lionrockjs/central';
-import config from './config/form.mjs';
-
-await Central.initConfig(new Map([
-  ['form', config],
-]));
+export default {}
